a0b40058125df24f04953a3fed8cebd34af2cdf7
Nguyen Minh Tài (QA) <82698590+minhtai1294@users.noreply.github.com>
2025-08-23 15:11:37 +0700

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -18,4 +18,4 @@ cat src/test/resources/dynamic-testng.xml
 
 # Step 2: Run Maven Test
 echo "=== Executing Tests ==="
-mvn test -DsuiteXmlFile="src/test/resources/dynamic-testng.xml" 
+mvn test -DsuiteXmlFile=src/test/resources/dynamic-testng.xml
